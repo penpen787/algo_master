d30e4eef22963b58921f002eb0159d68d2190c28
PenPen <penpen787@gmail.com>
2015-09-16 13:34:47 +0900

    Bubble Sorting

--- Algo_Master/src/algorithm/sort/bubble/BubbleSorting.java
+++ Algo_Master/src/algorithm/sort/bubble/BubbleSorting.java
@@ -1,6 +1,6 @@
 package algorithm.sort.bubble;
 
-import algorithm.sort.Algorithm;
+import algorithm.sort.common.Algorithm;
 
 /**
  * Bubble Sorting Algorithm
@@ -10,12 +10,37 @@ import algorithm.sort.Algorithm;
 public class BubbleSorting implements Algorithm {
 
 	@Override
-	public int[] solveIntegers(int[] input) {
+	public int[] solveIntegers(int[] arr) {
+
+		System.out.println("--- INPUT ---");
+		printIntArr(arr);
 		
+		int size = arr.length - 1;
 		
-		return null;
+		for(int i=0; i<size; i++) {
+			for(int j=0; j<(size-i); j++) {
+				if( arr[j] > arr[j+1] ) {
+					swap(arr, j, j+1);
+				}
+			}
+//			printIntArr(arr);
+		}
 		
+		return arr;
 	}
 
+	
+	public void swap(int[] arr, int a, int b) {
+		int tmp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = tmp;
+	}
+	
+	public void printIntArr(int[] arr) {
+		for(int a : arr) {
+			System.out.printf("%3d", a);
+		}
+		System.out.println();
+	}
 	
 }
--- Algo_Master/src/algorithm/sort/common/Algorithm.java
+++ Algo_Master/src/algorithm/sort/common/Algorithm.java
@@ -1,4 +1,4 @@
-package algorithm.sort;
+package algorithm.sort.common;
 
 public interface Algorithm {
 
