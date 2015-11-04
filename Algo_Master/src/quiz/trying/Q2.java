30737c42fec5bbdd08df0c5e28d6a24016994161
KH <KH>
2015-11-04 10:15:22 +0900

    [Q2][Solved]

--- Algo_Master/src/quiz/trying/Q2.java
+++ Algo_Master/src/quiz/trying/Q2.java
@@ -1,5 +1,7 @@
 package quiz.trying;
 
+import org.junit.Test;
+
 public class Q2 {
 	
 /**
@@ -8,5 +10,23 @@ public class Q2 {
  *  	- ex) for input N = 3, A is 4 (Q=1, R=1) and 8 (Q=2, R=2).
  *  		  So, the answer is 12 (4+8)
  *  	- N <= 2,000,000
+ *  
  */
+	
+	final int MAX = 2000000;
+	final int N = 7;
+	
+	/*
+	 *  My Answer. I think.. It's rather a math problem. when N=4, 4*1.. 1 -> 5,  4*2..2 -> 10, 4*3..3 -> 15
+	 *  So, Sigma (when n from 1 to (N-1)) (N+1)*n
+	 */
+	@Test
+	public void run() {
+		long sum = 0;
+		for(int i=1; i<N; i++) {
+			sum += (N+1) * i;
+		}
+		System.out.println(sum);
+	}
+	
 }
