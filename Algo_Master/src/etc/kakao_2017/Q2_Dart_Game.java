5fa8505045645c4ed7d7b836838b8421e674ca66
Kyunghun_Jeon <kh.jeon@navercorp.com>
2017-10-01 18:19:56 +0900

    카카오 신입공채 문제_2

---
+++ Algo_Master/src/etc/kakao_2017/Q2_Dart_Game.java
@@ -0,0 +1,106 @@
+package etc.kakao_2017;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+/**
+ * http://tech.kakao.com/2017/09/27/kakao-blind-recruitment-round-1/
+ * @author kyunghun.jeon
+ *         created on 2017. 10. 1
+ *        
+ * 풀이시간 : 00:43:18      
+ */
+public class Q2_Dart_Game {
+
+	public int getPoint(String dartResult) {
+
+		int[] points = new int[3];
+		char[] chars = dartResult.toCharArray();
+		
+		int dartIndex = 0;
+		int lastDartCharIndex = 0; 
+		char sc; // specialChar
+		
+		
+		for(int i=0; i<chars.length; i++) {
+			if (dartIndex == 4) {
+				break;
+			}
+			
+			char c = chars[i];
+
+			if (c == 'S' || c == 'D' || c == 'T') {
+
+				points[dartIndex] = getSquredPoints(Integer.valueOf(dartResult.substring(lastDartCharIndex, i)), c);
+				System.out.println(points[dartIndex]);
+
+				if (i != chars.length - 1) {
+					char nextChar = chars[i + 1];
+					if (nextChar == '*' || nextChar == '#') {
+						lastDartCharIndex = i + 2;
+						sc = nextChar;
+	
+						if (nextChar == '*') {
+							points[dartIndex] = points[dartIndex] * 2;
+							if(dartIndex > 0) {
+								points[dartIndex - 1] = points[dartIndex - 1] * 2;
+							}
+						} else {
+							points[dartIndex] = - points[dartIndex];
+						}
+						
+					} else {
+						lastDartCharIndex = i + 1;
+					}
+				}
+
+				dartIndex++;
+			}
+			
+		}
+
+		int sum = points[0] + points[1] + points[2];
+		System.out.println("intput : " + dartResult + " result : " + sum);
+
+		return sum;
+	}
+
+	private int getSquredPoints(Integer integer, char c) {
+		if (c == 'S') {
+			return integer;
+		} else if (c == 'D') {
+			return (int) Math.pow(integer, 2);
+		} else {
+			return (int) Math.pow(integer, 3);
+		}
+	}
+
+
+	@Test
+	public void test() {
+
+		String[] dartResults = {
+				"1S2D*3T",
+				"1D2S#10S",
+				"1D2S0T",
+				"1S*2T*3S",
+				"1D#2S*3S",
+				"1T2D3D#",
+				"1D2S3T*"};
+
+		int[] pointResults = {
+				37,
+				9,
+				3,
+				23,
+				5,
+				-4,
+				59};
+
+		for(int i=0; i<dartResults.length; i++) {
+			Assert.assertThat(this.getPoint(dartResults[i]), is(pointResults[i]));
+		}
+	}
+}
