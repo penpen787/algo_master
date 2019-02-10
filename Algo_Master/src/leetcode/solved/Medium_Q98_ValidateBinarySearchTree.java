d7fdf26c06b59e8360d30c92b8053872e2c5ec78
penpen787 <penpen787@gmail.com>
2019-02-10 17:42:35 +0900

    Leetcode Q98 Solved

---
+++ Algo_Master/src/leetcode/solved/Medium_Q98_ValidateBinarySearchTree.java
@@ -0,0 +1,22 @@
+package leetcode.solved;
+
+import leetcode.TreeNode;
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree
+ * assuming the value is between Integer range
+ */
+public class Medium_Q98_ValidateBinarySearchTree {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidNode(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidNode(TreeNode n, long min, long max) {
+        if(n == null) return true; // null tree is allowed
+
+        if(n.val <= min || n.val >= max) return false;
+
+        return isValidNode(n.left, min, n.val) && isValidNode(n.right, n.val, max);
+    }
+}
